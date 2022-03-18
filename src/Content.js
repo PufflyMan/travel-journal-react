74b603376f17dcdd419438fb09c5459cded2b133
PufflyMan <ahmedovyusif8785@gmail.com>
2022-03-19 02:41:25 +0400

    props

--- src/Content.js
+++ src/Content.js
@@ -4,12 +4,14 @@ import loc from './assets/location-icon.png';
 export default function Content(props) {
     return (
         <>
-            <div>
-                <img className="pic" src={props.item.imageUrl} width={125} alt={props.item.title} />
-                <h5>{props.item.location}</h5>
-                <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-                <p>{props.item.startDate} - {props.item.endDate}</p>
-                <p>{props.item.description}</p>
+            <div id="full-content">
+                <img className="pic" src={props.item.imageUrl} width={175} alt={props.item.title} />
+                <div id="content">
+                <h5 className="loc-name">{props.item.location}</h5>
+                <a className="link" href={props.item.googleMapsUrl}>View on Google Maps</a>
+                <p className="date">{props.item.startDate} - {props.item.endDate}</p>
+                <p className="desc">{props.item.description}</p>
+                </div>
 
                 {/* <img src={loc} alt='loc-icon' */}
             </div>
